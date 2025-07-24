d032a8a72aefdba096a570571ba00616530ca8d2
Guilherme Brandão <55606930+guibrandao@users.noreply.github.com>
2025-07-24 12:55:36 -0300

    Instanciando ACh Terms em First Order Terms

--- Exercicio.thy
+++ Exercicio.thy
@@ -10,7 +10,7 @@ theory Exercicio
 begin
 
 
-datatype \<F> = None string | AC | Hom
+datatype \<F> = Unin string | AC | Hom
 
 type_synonym \<V> = nat
 
@@ -25,8 +25,8 @@ locale ACh_properties =
 
 fun h_height:: "ach_term \<Rightarrow> nat" where (*Move*)
 "h_height (Var x) = 0"|
-"h_height (Fun (None f) []) = 0"|
-"h_height (Fun (None f) (t#ts)) = max (h_height t) (h_height (Fun (None f) ts))"|
+"h_height (Fun (Unin f) []) = 0"|
+"h_height (Fun (Unin f) (t#ts)) = max (h_height t) (h_height (Fun (Unin f) ts))"|
 "h_height (Fun AC []) = 0"|
 "h_height (Fun AC (t#ts)) = max (h_height t) (h_height (Fun AC ts))"
 
